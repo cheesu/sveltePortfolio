ce2bf0c68914fd793d6682b3eec760a31180b428
cheesu In <dlscltn@gmail.com>
2023-02-18 00:27:33 +0900

    테일윈드 레이아웃

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
+<h1>text-sm fkfkfkfkfk</h1>
