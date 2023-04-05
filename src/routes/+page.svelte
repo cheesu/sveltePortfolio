23c15947c0a0d4456d38c4afa38ad11871ca1cbf
cheesu In <dlscltn@gmail.com>
2023-04-05 14:41:08 +0900

    콘솔로그 뿌림

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,57 @@
   import PersonalPrj from '$components/main/PersonalPrj.svelte';
   import Parallax from '$components/main/Parallax.svelte';
   import CareerView from '$components/main/CareerView.svelte';
+
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    var color = {
+        brown: "color:#CC0000;font-size:11px;",
+        red: "color:#d83128;font-size:11px;",
+        yellow: "color:#ea991b;font-size:11px;",
+        green: "color:#93ba2f;font-size:11px;",
+        blue: "color:#4da4e0;font-size:11px;"
+    };
+
+    function isUA(userAgent:any):any {
+        return navigator.userAgent.toLowerCase().indexOf(userAgent) > -1;
+    }
+
+    if (typeof console === "object" && console.log && typeof console.log === "function") {
+        if (isUA('chrome') && !isUA('edge')) {
+            console.log(
+            "%c      ██╗███╗   ██╗    ███████╗████████╗██╗   ██╗██████╗ ██╗ ██████╗ \
+            \n%c%c      ██║████╗  ██║    ██╔════╝╚══██╔══╝██║   ██║██╔══██╗██║██╔═══██╗\
+            \n%c      ██║██╔██╗ ██║    ███████╗   ██║   ██║   ██║██║  ██║██║██║   ██║\
+            \n%c      ██║██║╚██╗██║    ╚════██║   ██║   ██║   ██║██║  ██║██║██║   ██║\
+            \n%c      ██║██║ ╚████║    ███████║   ██║   ╚██████╔╝██████╔╝██║╚██████╔╝\
+            \n%c      ╚═╝╚═╝  ╚═══╝    ╚══════╝   ╚═╝    ╚═════╝ ╚═════╝ ╚═╝ ╚═════╝ \
+            \n      %c%c   %cWell Come, My Site %cIN %cS%ct%cu%cd%ci%co.\n",     
+                color.brown, 
+                color.brown, color.red, color.yellow, color.green, color.blue, 
+                color.brown, color.red, color.yellow, color.green, color.blue, 
+                color.brown, color.red, color.yellow, color.green, color.blue, 
+                color.brown,
+            );  
+        } else {
+            console.log("%c   ____                                    \
+            \n%c  /$$$$$$                  /$$$$$$   /$$                     /$$ /$$          \
+            \n%c |_  $$_/                 /$$__  $$ | $$                    | $$|__/          \
+            \n%c   | $$   /$$$$$$$       | $$  \__//$$$$$$   /$$   /$$  /$$$$$$$ /$$  /$$$$$$ \
+            \n%c   | $$  | $$__  $$      |  $$$$$$|_  $$_/  | $$  | $$ /$$__  $$| $$ /$$__  $$\
+            \n%c   | $$  | $$  \ $$       \____  $$ | $$    | $$  | $$| $$  | $$| $$| $$  \ $$\
+            \n%c   | $$  | $$  | $$       /$$  \ $$ | $$ /$$| $$  | $$| $$  | $$| $$| $$  | $$\
+            \n%c  /$$$$$$| $$  | $$      |  $$$$$$/ |  $$$$/|  $$$$$$/|  $$$$$$$| $$|  $$$$$$/\
+            \n%c |______/|__/  |__/       \______/   \___/   \______/  \_______/|__/ \______/ \
+            \n      %cLife is too short, you need cou%cp%ca%cn%cg.\n"
+            );
+        }
+        console.log("\x3e \x3e \x3e https://cheesu.com\n")
+    }
+  
+  });
+
+  
 </script>
 
   <div>
