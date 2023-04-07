12cdcf26f3a1772b150eb924c15c0e3c10f0a5e1
cheesu In <dlscltn@gmail.com>
2023-04-07 15:39:24 +0900

    블로그 이미지 미리보기 추가

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -1,23 +1,48 @@
 import { CLIENT_ID , SC_KEY, TISTORY_ACTK} from '$env/static/private';
-
+import axios from 'axios';
+import { load as cheerioLoad } from 'cheerio';
 import type { PageServerLoad } from './$types';
+
  
 export const load: PageServerLoad = async function () {
 	//const test:string = CLIENT_ID;
     //const key:string = SC_KEY;
       const response = await fetch('https://www.tistory.com/apis/post/list?access_token='+TISTORY_ACTK+'&output=json&blogName=yamea-guide&page=1');
-      
+      const responseBlogInfo = await fetch('https://www.tistory.com/apis/blog/info?access_token='+TISTORY_ACTK+'&output=json');
       let data = await response.json();
-
       let postArr = data.tistory.item.posts;
+
+      
+      let blogData = await responseBlogInfo.json();
+      let blogInfo = blogData.tistory.item;
+
+      console.log(blogInfo[0])
+      //console.log(Object.keys(blogInfo[0]));
+
+      
 
       // 발행상태인 포스트만 공개 한다.
       postArr = postArr.filter((item:any)=>{
         return item.visibility === "20"
       });
 
+
+      // 발행상태인 포스트의 오픈그래프 이미지 메타태그를 찾는다.
+      const postArrWithImages = await Promise.all(
+        postArr.map(async (item: any) => {
+          const response = await axios.get(item.postUrl);
+          const $ = cheerioLoad(response.data);
+          // 오픈그래프 이미지 메타 태그를 찾습니다.
+          const previewImage = $('meta[property="og:image"]').attr('content');
+          return { ...item, image: previewImage };
+        })
+      );
+
+      postArrWithImages;
+
 	return {
-        postArr:postArr
+        postArr:postArrWithImages,
+        blogData: blogInfo
 	};
 };
 
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,10 +1,41 @@
 <script lang="ts">
+  
+
     import CardList from '$components/card/CardList.svelte';
     import type { PageData } from './$types';
 	  export let data: PageData;
-    $: ({ postArr} = data);
+    
 
   console.log(data.postArr);
+  console.log(data.blogData.blogs[0]);
+
+  const blogInfo:blogInfo = data.blogData.blogs[0];
+
+  interface blogInfo {
+      name: string;
+      url: string;
+      secondaryUrl: string;
+      nickname: string;
+      title: string;
+      description: string;
+      default: string;
+      blogIconUrl: string;
+      faviconUrl: string;
+      profileThumbnailImageUrl: string;
+      profileImageUrl: string;
+      role: string;
+      blogId: string;
+      isEmpty: string;
+      statistics: statistics;
+    }
+
+  interface statistics{
+    comment: string;
+    guestbook: string;
+    post: string;
+    trackback: string;
+  }
+  
 
   interface postItem {
     title: string;
@@ -12,6 +43,7 @@
     description: string;
     imageUrl: string;
   }
+
 
   let cards:postItem[];
 
@@ -21,13 +53,26 @@
         title : "",
         postUrl: item.postUrl,
         description: item.title,
-        imageUrl: 'https://picsum.photos/id/1/300/200',
+        imageUrl: item.image,
        }
        return postData;
   });
 
   </script>
 
+  <h2 class="mt-20 w-full text-center ">블로그 소개</h2>
+    
+  <div>
+    <div class=" w-1/4">
+      <img class="object-cover" src={blogInfo.profileImageUrl} alt="Blog profileImage" />
+    </div>
+    {blogInfo.name}<br>
+    {blogInfo.description}<br>
+    <span>post : </span>{blogInfo.statistics.post} <br>
+    <span>comment : </span>{blogInfo.statistics.comment} <br>
+    <span>guestbook : </span>{blogInfo.statistics.guestbook} <br>
+    <span>trackback : </span>{blogInfo.statistics.trackback} <br>
+  </div>
 
   <div class="h-screen">
     <h1 class="text-20 p-10">최근 포스팅한 게시물</h1>
